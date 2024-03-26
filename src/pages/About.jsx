495b1abd9b6b00f8bd8c4cbf3bf035b9e6c9afef
Kris10 <kmrichie611@gmail.com>
2024-03-26 15:37:20 -0500

    updated about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
             <p>I am a full-stack developer who has a passion for turning a business's vision into a reality. <br />With more people online today, I think it is important that a business's
                     website reflects its values, mission, and services. Equally important is the functionality of a
                     business's website so that users and clients have a seamless experience while browsing. <br />I am excited
-                    at the prospect of making your vision a reality. Scroll below to view my past projects and the services
-                    that I can offer to you and your business.</p>
+                    at the prospect of making your vision a reality. To view my past projects and the services
+                    that I can offer to you and your business, navigate to portfolio section above!</p>
             </div>
 
         </div>
